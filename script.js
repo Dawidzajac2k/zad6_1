47f8cb2830882693d41f3b38c77d587fbc46fa89
dawidzajac2k <100692159+Dawidzajac2k@users.noreply.github.com>
2022-05-24 11:46:12 +0000

    6_2

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 (function () {
    const exbutton = document.getElementById('ex1_content')
    const excontent = document.getElementById('ex1_button')
-
+   const extext = document.getElementById('ex2_text')
+  
   excontent.addEventListener("click",function (){
   var data="0,1,2,3,4,5,6,7,8,9";
  
     exbutton.innerHTML=data;
+  })
+  extext.addEventListener("input",function (){
+  const popr = "Długość numeru musi być równa 9"
+  var nr = document.getElementById("ex2_text");
+  const log = document.getElementById('ex2_content');
+    nr.addEventListener("imput", function(){
+      
+      if (nr.value.match(/^[0-9]+$/) && nr.value.lenght ==9)
+        log.textContent = "Numer telefon jest poprawny";
+      
+    });
+   
+                                         
   })
 })();
